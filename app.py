99dad8532250d1cecc8e764b2f3da38c1bf9a9b3
Brian Le <briandle00@gmail.com>
2019-11-10 10:07:04 -0800

    modifying app.py and changing to param queries

--- app.py
+++ app.py
@@ -2,14 +2,31 @@ from flask import Flask, jsonify
 from flask_restful import Api
 from flask_pymongo import PyMongo
 
-from resources.news import QuestionCreator, Question, QuestionList
+from resources.news import News
 
 app = Flask(__name__)
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/kahoot'
-api = Api(app)
+#api = Api(app)
 
-api.add_resource(NewsCreator, '/createnews')
+#api.add_resource(News, '/newsoutput')
+@app.route('/newsoutput')
+def test():
+	print('ok')
+	return 'ok'
 
+
+def get(country, sources, q, source_from, sortby):
+    url = ('https://newsapi.org/v2/everything?'
+           'q='+q+'&'
+           'from='+source_from+'&'
+           'sortBy='+sortby+'&'
+           'apiKey=cc2092da085c4fc3897fbb74e2e41ca8')
+    return 'ok'
+    try:
+        response = requests.get(url)
+        return r.json, 200
+    except:
+        return {'message': 'An error occured trying to look up this news'}, 500
 
 if __name__ == '__main__':
     from db import mongo
